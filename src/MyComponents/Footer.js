9a6eaa4e59fd31f0d5e1bf5302ed3e32fe596ae7
Samarth H Chinivar <samarthharish321@gmail.com>
2023-03-06 15:52:08 +0530

    footer updated

--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -1,19 +1,11 @@
 import React from 'react'
 
-const Footer = () => {
-  //  let footerStyle = {
-  //   position: "relative",
-  //   top: "8vh",
-  //   width: "100%",
-  //   border: '2px solid'
-  // }
-  
+const Footer = () => {  
   return (
     // <footer className='bg-dark text-white' style={footerStyle}>
     <footer className='bg-dark text-white'>
       <p className="text-center">
-        &copy; Samarth H Chinivar. <br /> 
-        <a href='https://samarth-portfolio-website.000webhostapp.com' target='_blank'>Contact me</a>
+        <a href='https://samarth-portfolio-website.000webhostapp.com' target='_blank' rel="noopener">&copy; Samarth H Chinivar.</a>
       </p>
     </footer>
   )
